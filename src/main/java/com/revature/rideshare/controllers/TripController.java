b53cb2077f9260b9607e3ae046ab8754c151cf49
Jean Lucas <jean@4ray.co>
2020-05-03 19:17:24 -0400

    Add Trip controller and service tests

--- src/main/java/com/revature/rideshare/controllers/TripController.java
+++ src/main/java/com/revature/rideshare/controllers/TripController.java
@@ -1,35 +1,23 @@
 package com.revature.rideshare.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.rideshare.models.Trip;
 import com.revature.rideshare.models.TripDTO;
 import com.revature.rideshare.models.User;
 import com.revature.rideshare.services.TripService;
 import com.revature.rideshare.services.UserService;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
