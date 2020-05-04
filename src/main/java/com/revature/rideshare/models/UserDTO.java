4817d7b7af4e9190d774bb86f5d352d6c53fa482
Gilbert Turner <grturner@5x5code.com>
2020-05-04 11:56:39 -0400

    User model updates Updated user model and dto to reflect changes in dev

--- src/main/java/com/revature/rideshare/models/UserDTO.java
+++ src/main/java/com/revature/rideshare/models/UserDTO.java
@@ -1,74 +1,71 @@
 package com.revature.rideshare.models;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 public class UserDTO {
-    private int userId;
 
-    @NotBlank(message="Username cannot be blank.")
-    @Size(min=3,max=12, message="Number of characters must be between 3 and 12.")
-    @Pattern(regexp="[a-zA-Z0-9]+", message="Username may only have letters and numbers.")
-    private String userName;
+	private int userId;
 
-    @Valid
-    @NotNull
-    private BatchDTO batch;
+	@NotBlank(message = "Username cannot be blank.")
+	@Size(min = 3, max = 12, message = "Number of characters must be between 3 and 12.")
+	@Pattern(regexp = "[a-zA-Z0-9]+", message = "Username may only have letters and numbers.")
+	private String userName;
 
-    @NotBlank(message="First name cannot be blank.")
-    @Size(max=30, message= "Number of characters cannot be larger than 30.")
-    @Pattern(regexp="^[a-zA-Z\\u00C0-\\u017F]+[- ]?[a-zA-Z\\u00C0-\\u017F]+$", message="First name format is incorrect")
-    private String firstName;
+	@Valid
+	@NotNull
+	private BatchDTO batch;
 
-    @NotBlank(message="Last name cannot be blank.")
-    @Size(max=30, message="Number of characters cannot be larger than 30.")
-    @Pattern(regexp="^[a-zA-Z\\u00C0-\\u017F]+[- ]?[a-zA-Z\\u00C0-\\u017F]+$", message="Last name format is incorrect")
-    private String lastName;
+	@NotBlank(message = "First name cannot be blank.")
+	@Size(max = 30, message = "Number of characters cannot be larger than 30.")
+	@Pattern(regexp = "^[a-zA-Z\\u00C0-\\u017F]+[- ]?[a-zA-Z\\u00C0-\\u017F]+$", message = "First name format is incorrect")
+	private String firstName;
 
-    @NotBlank(message="Email cannot be blank.")
-    @Email(message="Email format is incorrect.")
-    private String email;
+	@NotBlank(message = "Last name cannot be blank.")
+	@Size(max = 30, message = "Number of characters cannot be larger than 30.")
+	@Pattern(regexp = "^[a-zA-Z\\u00C0-\\u017F]+[- ]?[a-zA-Z\\u00C0-\\u017F]+$", message = "Last name format is incorrect")
+	private String lastName;
 
-    @NotBlank(message="Phone number cannot be blank.")
-    @Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$", message="Phone number format is incorrect.")
-    private String phoneNumber;
+	@NotBlank(message = "Email cannot be blank.")
+	@Email(message = "Email format is incorrect.")
+	private String email;
 
-    private boolean isDriver;
+	@NotBlank(message = "Phone number cannot be blank.")
+	@Pattern(regexp = "^\\d{3}-\\d{3}-\\d{4}$", message = "Phone number format is incorrect.")
+	private String phoneNumber;
 
-    private boolean isActive;
+	private boolean driver;
 
-    private boolean isAcceptingRides;
+	private boolean active;
 
-    @Valid
-    private AddressDTO hAddress;
+	private boolean acceptingRides;
 
-    @Valid
-    private AddressDTO wAddress;
+	@Valid
+	private AddressDTO homeAddress;
 
-    public UserDTO(User user) {
-        super();
-        if (user != null) {
-            this.userId = user.getUserId();
-            this.userName = user.getUserName();
-            this.batch = new BatchDTO(user.getBatch());
-            this.firstName = user.getFirstName();
-            this.lastName = user.getLastName();
-            this.email = user.getEmail();
-            this.phoneNumber = user.getPhoneNumber();
-            this.isDriver = user.isDriver();
-            this.isActive = user.isActive();
-            this.isAcceptingRides = user.isAcceptingRides();
-            this.hAddress = new AddressDTO(user.getHAddress());
-            this.wAddress = new AddressDTO(user.getWAddress());
-        }
-    }
+	@Valid
+	private AddressDTO workAddress;
+
+	public UserDTO(User user) {
+		super();
+		if (user != null) {
+			this.userId = user.getUserId();
+			this.userName = user.getUserName();
+			this.batch = new BatchDTO(user.getBatch());
+			this.firstName = user.getFirstName();
+			this.lastName = user.getLastName();
+			this.email = user.getEmail();
+			this.phoneNumber = user.getPhoneNumber();
+			this.driver = user.isDriver();
+			this.active = user.isActive();
+			this.acceptingRides = user.isAcceptingRides();
+			this.homeAddress = new AddressDTO(user.getHomeAddress());
+			this.workAddress = new AddressDTO(user.getWorkAddress());
+		}
+	}
 }
