5168ee635fb16db49cf49945a23c9a4d91a1f8e8
Gilbert Turner <grturner@5x5code.com>
2020-05-03 20:26:38 -0400

    Added forgotten annotations

--- src/main/java/com/revature/rideshare/TripServiceApplication.java
+++ src/main/java/com/revature/rideshare/TripServiceApplication.java
@@ -3,9 +3,13 @@ package com.revature.rideshare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
+@EnableAspectJAutoProxy
 public class TripServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TripServiceApplication.class, args);
